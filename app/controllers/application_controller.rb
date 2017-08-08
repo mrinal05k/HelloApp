163f2d054eb361fa81af280dc4270906e8a909d9
Mrinal Kumar <mrinalkumar@Mrinals-MacBook-Air.local>
2017-08-09 00:00:20 +0530

    code change for fun

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def Hello
-  	render text: "Hello, World!"
+  	render text: "Hello, World"
   	
   end
 end
